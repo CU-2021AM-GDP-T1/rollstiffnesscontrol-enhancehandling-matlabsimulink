b097f17d8a7b20c45ef728d7a36c4c58128abdb8
CCTJB2 <jasoncheung54@gmail.com>
2022-04-04 17:25:33 +0100

    Add simulink cache and code generation files

---
+++ CM_GDP_CU/src_cm4sl/slprj/raccel/untitled/untitled_private.h
@@ -0,0 +1,30 @@
+#ifndef RTW_HEADER_untitled_private_h_
+#define RTW_HEADER_untitled_private_h_
+#include "rtwtypes.h"
+#include "builtin_typeid_types.h"
+#include "multiword_types.h"
+#include "zero_crossing_types.h"
+#if !defined(rt_VALIDATE_MEMORY)
+#define rt_VALIDATE_MEMORY(S, ptr)   if(!(ptr)) {\
+  ssSetErrorStatus(rtS, RT_MEMORY_ALLOCATION_ERROR);\
+  }
+#endif
+#if !defined(rt_FREE)
+#if !defined(_WIN32)
+#define rt_FREE(ptr)   if((ptr) != (NULL)) {\
+  free((ptr));\
+  (ptr) = (NULL);\
+  }
+#else
+#define rt_FREE(ptr)   if((ptr) != (NULL)) {\
+  free((void *)(ptr));\
+  (ptr) = (NULL);\
+  }
+#endif
+#endif
+extern real_T look1_pbinlcapw ( real_T u0 , const real_T bp0 [ ] , const
+real_T table [ ] , uint32_T prevIndex [ ] , uint32_T maxIndex ) ;
+#if defined(MULTITASKING)
+#error Models using the variable step solvers cannot define MULTITASKING
+#endif
+#endif
